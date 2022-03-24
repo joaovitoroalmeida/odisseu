fc177eadbb0ee127c07e96ffdf6fc01be8421fe6
joao-vitor-developer <jv.oa202@gmail.com>
2022-03-23 22:38:46 -0300

    update project with first unit test

---
+++ usecase/src/test/kotlin/helpcar/mockutils/CustomerMockUtils.kt
@@ -0,0 +1,17 @@
+package helpcar.mockutils
+
+import helpcar.api.domain.Customer
+
+fun buildCustomer(
+    customerId: String = "123",
+    phone: String = "11987963254",
+    email: String = "joao.oliveira@gmail.com",
+    cpf: String = "37975804010",
+    name: String = "Irineu Batista"
+) = Customer(
+    customerId = customerId,
+    phone = phone,
+    email = email,
+    cpf = cpf,
+    name = name
+)

79418b7b7e2c7e70a7504025e42736be3cd8187f
joao-vitor-developer <jv.oa202@gmail.com>
2022-03-23 22:36:49 -0300

    pitest implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ val kotlinCoroutinesVersion: String by ext
 val jacksonVersion: String by ext
 val springBootStarterVersion: String by ext
 val mockkVersion: String by ext
+val pitestVersion: String by ext
+val pitestJunit5Version: String by ext
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 
@@ -15,6 +17,7 @@ plugins {
 
 	id("org.springframework.boot") version "2.6.4" apply false
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("info.solidsoft.pitest") version "1.4.7"
 }
 
 allprojects {
@@ -33,6 +36,7 @@ subprojects {
 		plugin("kotlin-spring")
 
 		plugin("io.spring.dependency-management")
+		plugin("info.solidsoft.pitest")
 
 		plugin("idea")
 	}
@@ -52,6 +56,10 @@ subprojects {
 
 			// test
 			dependency("io.mockk:mockk:$mockkVersion")
+
+			// pitest
+			dependency("info.solidsoft.gradle.pitest:gradle-pitest-plugin:$pitestVersion")
+			dependency("org.pitest:pitest-junit5-plugin:$pitestJunit5Version")
 		}
 	}
 
@@ -66,6 +74,10 @@ subprojects {
 		// spring
 		implementation("org.springframework.boot:spring-boot-starter-web")
 
+		// pitest
+		implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin")
+		implementation("org.pitest:pitest-junit5-plugin")
+
 		// test
 		testImplementation("io.mockk:mockk")
 		testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -79,6 +91,12 @@ subprojects {
 				freeCompilerArgs = listOf("-Xjsr305=strict")
 				jvmTarget = "11"
 			}
+		}
+
+		pitest {
+			testPlugin.set("junit5")
+			outputFormats.set(setOf("HTML"))
+			mutators.set(setOf("STRONGER", "DEFAULTS"))
 		}
 
 		test {
